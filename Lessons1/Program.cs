b5ec363a263a4a39437ca0ea10a80875d7bc924d
shura.aa <suraehmedov37@gmail.com>
2025-03-25 19:27:10 +0400

    create lessons1

---
+++ Lessons1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Lessons1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salam Dunya");
+
+       
+
+        }
+    }
+}
